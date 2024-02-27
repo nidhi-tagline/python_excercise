7ba65dfbeb2ba603205f743d86fe1cf983bd2e66
nidhi tagline <nidhig.tagline@gmail.com>
2024-02-27 09:49:42 +0530

    changes in task 10,11

--- task10.py
+++ task10.py
@@ -5,54 +5,58 @@
 # *   *
 # ** **
 
-for i in range(2):
-    for j in range(2-i,0,-1):
+n = 2
+for i in range(n):
+    for j in range(n-i,0,-1):
         print('*',end='')
     for _ in range(i+1):
         print('  ',end='')
-    for j in range(2-i,0,-1):
+    for j in range(n-i,0,-1):
         print('*',end='')
     print()
-    
-for i in range(3):
+for i in range(n+1):
     for j in range(i):
         print('*',end='')
-    for _ in range(2-i,-1,-1):
+    for _ in range(n-i,-1,-1):
         print('  ',end='')
     for j in range(i):
         print('*',end='')
     print()
 
 print()
+
 #   *  
 #  *** 
 # *****
 #  *** 
 #   *  
- 
-for i in range(1, 4):
-    for _ in range(3-i):
+
+n = 3
+for i in range(1,n+1):
+    for _ in range(n-i):
         print(' ',end='')
     for j in  range(2*i - 1):
         print('*',end='')
     print()
-    
-for i in range(1,3):    
+for i in range(1,n):    
     for _ in range(i):
         print(' ',end='')
-    for j in range(2*(2-i)+1):
+    for j in range(2*((n-1)-i)+1):
         print('*',end='')
     print()
     
 print()
+
 # *
 # * *
 # *   *
 # *     *
 # * * * * *   
-for i in range(1,6):
+
+n = 5
+for i in range(1,n+1):
     for j in range(i):
-        if j == i-1 or j == 0 or i == 5:
+        if j == i-1 or j == 0 or i == n:
             print('* ',end='')
         else:
             print(end='  ')
@@ -60,27 +64,32 @@ for i in range(1,6):
     print()
  
 print()  
+
 # * * * * *
 # *       *
 # *       *
 # *       * 
 # * * * * *
-for i in range(5):
-    for j in range(5):
-        if i == 0 or i == 4 or j == 0 or j == 4:
+
+n=5
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n-1 or j == 0 or j == n-1:
             print(end='* ')
         else:
             print(end='  ')
     print()
 
 print()
+
 # 1
 # 2 3
 # 4 5 6
 # 7 8 9 10
 # 11 12 13 14 15
-a = 1
-for i in range(5):
+
+a,n = 1,5
+for i in range(n):
     for j in range(i+1):
         print(end=f"{a} ")
         a+=1
